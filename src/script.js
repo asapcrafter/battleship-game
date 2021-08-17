7dab5fe9c60bad2294c307f9e9146240639e7595
Steve <asapcrafter@gmail.com>
2021-08-16 23:52:07 -0700

    added modules

--- src/script.js
+++ src/script.js
@@ -1,16 +1,19 @@
 const Ship = function Ship(length, ...coords) {
-    //Set location object property
     const Location = function Location(coord) {
         const area = coord;
-        let status = 1;
+        let status = 1; //Status: (Alive = 1, Dead = 0)
         return {area, status};
     };
+
     let location = [];
         coords.forEach(e => location.push(e = Location(e)));
+
     const hit = (coord) => {
+        if (coords.find(e => e === coord) == undefined) return;
         hitArea = location.filter(e => e.area == coord);
         hitArea[0].status = 0;
     };
+
     //Checks for Ship's overall health
     const getHealth = () => {
         let health = [];
@@ -21,40 +24,72 @@ const Ship = function Ship(length, ...coords) {
     //Checks for Ship's individual area health
     const getStatus = (coord) => {
         const area = location.filter(e => e.area == coord);
-        console.log(`Area:${area[0].area}'s hp is ${area[0].status}`);
+        console.log(`${area[0].area}'s health is ${area[0].status}`);
         return area[0].status;
     }; 
     const isSunk = () => {
         if (getHealth() === 0) console.log("Ship is sunk");
         return (getHealth() === 0);  
     };
+
     return {getHealth, getStatus, isSunk, hit, location, coords};
-}     
+} 
+
 const Gameboard = function Gameboard() {
     let totalShips = []; //stores all Ship objects 
+
     const makeShip = (length, ...coords) => {
         newShip = Ship(length, ...coords);
         totalShips.push(newShip);
     };
-    let validCoords = [] //stores all Ship locations
-    const getValidCoords = () => {
+
+    //Checks if a grid coordinate contains a Ship
+    const getValidCoords = () => { 
+        let validCoords = []; 
         totalShips.forEach(e => validCoords.push(e.coords));
         validCoords = validCoords.flat();
         return validCoords;
     };
+    const checkIfValidCoord = (coord) => {
+        const coordArray = getValidCoords();
+        const matchingCoord = coordArray.find(e => e == coord)
+        return (matchingCoord !== undefined) 
+    };
+
+    let missedCoords = []; //stores all missed shots
+    let hitCoords = []; //stores all successful shots
+
     const receiveAttack = (coord) => {
-        if (Ship.status === 0) {
-            //already hit
-        }
-        else if (coord === Ship.getLocation()) {
-            Ship.hit()
-            //successful hit
-        } else {
-            //missed 
-        }
-    }
-    return {totalShips, makeShip, getValidCoords};
+        if (checkIfValidCoord(coord) === false) {
+            missedCoords.push(coord);
+            console.log("Missed hit")
+            return;
+        };
+        if (hitCoords.find(e => e == coord) !== undefined) {
+            console.log("Already hit")
+            return;
+        };
+        const matchingShip = totalShips.find(e => {
+            return e.coords.includes(coord);
+        });
+        matchingShip.hit(coord);
+        hitCoords.push(coord);
+    };
+
+    const checkFleetHP = () => {
+        let fleetHealth = [];
+        totalShips.forEach(e => fleetHealth.push(e.getHealth()));
+        fleetHealth = fleetHealth.flat();
+        fleetHealth = fleetHealth.reduce((sum, amount) => sum + amount);
+        return fleetHealth;
+    };
+    const checkFleetSunk = () => {
+        return checkFleetHP() === 0;
+    };
+
+    return {totalShips, makeShip, getValidCoords, checkIfValidCoord, receiveAttack, checkFleetHP, checkFleetSunk};
 }
+
 //Generates all possible coordinates in a 10x10 
 const renderCoords = function renderCoordinates() {
     let coordArray = [];
@@ -74,7 +109,7 @@ const renderCoords = function renderCoordinates() {
 // console.log(shipOne)
 
 
-// shipOne.hit('A1')
+// shipOne.hit('A5')
 // shipOne.hit('A3')
 // console.log(shipOne.location)
 // console.log(shipOne.getHealth())
@@ -82,6 +117,9 @@ const renderCoords = function renderCoordinates() {
 const testBoard = Gameboard()
 testBoard.makeShip(2, 'A1', 'B2', 'A3' )
 testBoard.makeShip(5, 'C1', 'C2', 'C3' )
+// console.log(testBoard.getValidCoords())
+
+console.log(testBoard.receiveAttack('A1'))
 console.log(testBoard.totalShips)
-console.log(testBoard.getValidCoords())
+// console.log(testBoard.totalShips[0].coords.includes('A1'))
 
